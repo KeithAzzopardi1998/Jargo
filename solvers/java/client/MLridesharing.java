4e18f7fffe0bd78b221e9e995905e3555271648c
Keith Azzopardi <keith.azzopardi.16@um.edu.mt>
2020-11-10 19:09:16 +0100

    starting to lay out the optimization module

--- solvers/java/client/MLridesharing.java
+++ solvers/java/client/MLridesharing.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.HashMap;
 import java.util.Collections;
-
+import java.util.stream.IntStream;
 import java.util.Random;
 
 public class MLridesharing extends Client {
@@ -45,6 +45,7 @@ public class MLridesharing extends Client {
     }
 
     //3. call optimization module
+    int[] matches = optimizationModule(cm, c);
 
     //4. update vehicle routes
     
@@ -107,11 +108,50 @@ public class MLridesharing extends Client {
     return costs;
   }
 
-  protected Integer[] optimizationModule(final Integer[] contextMapping, final Integer[][] costs) {
+  protected int[] optimizationModule(final Integer[][] contextMapping, final Integer[][] costs) {
     //we shall store an array with one entry for each request
     //the value corresponds to the vehicle ID
-    Integer matches = new Integer[contextMapping.length];
+    Integer[] matches = new Integer[contextMapping.length];
+ 
+    int num_customers=contextMapping.length;
 
-    
+    int[] vehicle_list=new int[num_customers*contextMapping[0].length];
+    for (int i = 0; i < contextMapping.length; i++) {
+        // tiny change 1: proper dimensions
+        for (int j = 0; j < contextMapping[0].length; j++) { 
+            // tiny change 2: actually store the values
+            vehicle_list[(i*contextMapping[0].length) + j] = contextMapping[i][j]; 
+        }
+    }
+
+    //getting a unique list of vehicles
+    int[] vehicles_unique = IntStream.of(vehicle_list).distinct().toArray();
+    int num_vehicles=vehicles_unique.length;
+    if (DEBUG) {
+      System.out.println("Unique list of vehicles\n");
+      System.out.println(Arrays.toString(vehicles_unique));
+    }
+
+    //building the matrix which will eventually be passed to the matching algorithmbg
+    Integer[][] weight_matrix = new Integer[num_customers][num_vehicles];
+    //looping through the customers
+    for (int i = 0; i < num_customers; i++) {
+      //looping through the vehicles
+      for (int j = 0; j < num_vehicles; j++) {
+        int vehicle_id = vehicles_unique[j];
+
+        //check if this vehicle can serve this customer (from the context mapping module)
+        if (Arrays.stream(contextMapping[i]).anyMatch(x -> x == vehicle_id) ) {
+          //IDEA: calculate the cost here
+        } else {
+          //if the vehicle cannot serve this customer, set the weight to infinity 
+          weight_matrix[i][j]=Integer.MAX_VALUE;
+        }
+
+      }
+    }
+
+    return vehicles_unique;
+
   }
 }
