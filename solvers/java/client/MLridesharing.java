2c7c098260b70d4c65930990149fdede4fab97a5
Keith Azzopardi <keith.azzopardi.16@um.edu.mt>
2020-11-12 19:30:51 +0100

    adding the basic logic for the customer-vehicle assignment

--- solvers/java/client/MLridesharing.java
+++ solvers/java/client/MLridesharing.java
@@ -17,7 +17,12 @@ public class MLridesharing extends Client {
     System.out.printf("Set MAX_PROXIMITY=%d\n", MAX_PROXIMITY);
     this.batch_processing=true;
   }
-  protected void handleRequestBatch(Object[] rb) throws ClientException, ClientFatalException {
+
+  //used to process a batch of requests rb
+  //NOTE: the order of rb must be preserved at all times
+  //      example: in the cost matrix and context map, there is one row
+  //      for each request in rb
+  protected void handleRequestBatch(final Object[] rb) throws ClientException, ClientFatalException {
     if (DEBUG) {
       System.out.printf("processing batch of size %d\n", rb.length);
     }
@@ -26,39 +31,94 @@ public class MLridesharing extends Client {
       return; 
     }
 
+    //now that we have a copy of the request batch in rb, we can clear the queue
+    //any unserved requests are added back once we know that they cannot be served
+    this.queue.clear();
+
     //1. call context mapping module
-    Integer[][] cm = new Integer[rb.length][20];
-    cm = contextMappingModule(rb);
+    Integer[][] context_map = new Integer[rb.length][20];
+    context_map = contextMappingModule(rb);
     if (DEBUG) {
       System.out.printf("Context Map: \n");
-      for(Integer[] x: cm)
+      for(Integer[] x: context_map)
             System.out.println(Arrays.toString(x));  
       System.out.printf("\n-----------------------\n\n");
     }
 
+    //getting a unique list of vehicles of interest by first flattening out the context map, and then
+    //adding to an array of unique items
+    int[] vehicle_list=new int[context_map.length*context_map[0].length];
+    for (int i = 0; i < context_map.length; i++) {
+        // tiny change 1: proper dimensions
+        for (int j = 0; j < context_map[0].length; j++) { 
+            // tiny change 2: actually store the values
+            vehicle_list[(i*context_map[0].length) + j] = context_map[i][j]; 
+        }
+    }
+    int[] vehicles = IntStream.of(vehicle_list).distinct().toArray();
+    if (DEBUG) {
+      System.out.println("Unique list of vehicles\n");
+      System.out.println(Arrays.toString(vehicles));
+      System.out.printf("\n-----------------------\n\n");
+    }
+
     //2. ask vehicles for insertion costs and form the cost matrix
-    double[][] c = getCostMatrix(rb,cm);
+    double[][] cost_matrix = getCostMatrix(rb,vehicles,context_map);
     if (DEBUG) {
       System.out.printf("Insertion Costs: \n");
-      for(double[] x: c)
+      for(double[] x: cost_matrix)
             System.out.println(Arrays.toString(x));  
       System.out.printf("\n-----------------------\n\n");
     }
 
     //3. call optimization module
-    HungarianAlgorithm optModule = new HungarianAlgorithm(c);
+    HungarianAlgorithm optModule = new HungarianAlgorithm(cost_matrix);
     int[] assignments = optModule.execute();
-    System.out.printf("assignments: \n");
-    System.out.println(Arrays.toString(assignments));  
-    System.out.printf("\n-----------------------\n\n");
-
-    //4. update vehicle routes
+    if (DEBUG) {
+      System.out.printf("Assignments: \n");
+      System.out.println(Arrays.toString(assignments));  
+      System.out.printf("\n-----------------------\n\n");
+    }
     
-    this.queue.clear();
-    //TODO if a request is rejected by the context mapping module, it must be left in the queue
+    //4. update vehicle routes
+    //TODO ensure that the length of the assignments array is equal to the number of requests
+    //loop through the request
+    for (int i = 0; i < assignments.length; i++ ){
+      //the info about the request to be inserted
+      //is obtained from rb (recall that the order of "assignments"
+      //MUST follow the order of rb)
+      Object r = rb[i];
+
+      //NOTE: the "assignments" array does not hold vehicle IDs which can be 
+      //      used directly. It contains the index of that vehicle inside
+      //      the "vehicles" array. Therefore, we first fetch that index,
+      //      check if it is -1 (which means that the request should not be assigned to any vehicle)
+      //      and if not, proceed to fetch the actual vehicle ID and insert it into the route
+      int v_index = assignments[i];
+      if (v_index == -1) {
+        //add the request back to the queue so that we can try to insert it in the next epoch
+        if (DEBUG) {
+          System.out.printf("Adding request %d to queue\n",i);
+        }
+        //TODO: this is where the reactive repositioning should probably come in too
+      }
+      else {
+        int v_id = vehicles[v_index];
+        if (DEBUG) {
+          System.out.printf("Adding request %d to vehicle with ID %d\n",i,v_id);
+        }
+        addToRoute(r,v_id);
+      }
+    }
+    if (DEBUG) {
+      System.out.printf("---------finished processing batch---------\n");
+    }
+
+
+    
   }
 
-  //performs the Context Mapping describe in Simonetto 2019
+  //performs the Context Mapping as described in Simonetto 2019
   protected Integer[][] contextMappingModule(final Object[] rb) {
     
     Integer [][] contextMapping = new Integer[rb.length][20];
@@ -96,25 +156,9 @@ public class MLridesharing extends Client {
 
   //takes the list of requests and output of the context mapping module to
   //form the cost matrix by asking the vehicles for the insertion cost
-  protected double [][] getCostMatrix(final Object[] rb, final Integer[][] contextMapping ) {
+  protected double [][] getCostMatrix(final Object[] rb, final int [] vehicles, final Integer[][] contextMapping ) {
     int num_customers=rb.length;
-
-    //getting a unique list of vehicles by first flattening out the context map, and then
-    //adding to an array of unique items
-    int[] vehicle_list=new int[num_customers*contextMapping[0].length];
-    for (int i = 0; i < contextMapping.length; i++) {
-        // tiny change 1: proper dimensions
-        for (int j = 0; j < contextMapping[0].length; j++) { 
-            // tiny change 2: actually store the values
-            vehicle_list[(i*contextMapping[0].length) + j] = contextMapping[i][j]; 
-        }
-    }
-    int[] vehicles = IntStream.of(vehicle_list).distinct().toArray();
     int num_vehicles=vehicles.length;
-    if (DEBUG) {
-      System.out.println("Unique list of vehicles\n");
-      System.out.println(Arrays.toString(vehicles));
-    }
 
     //building the matrix which will eventually be passed to the matching algorithmbg
     double[][] weight_matrix = new double[num_customers][num_vehicles];
@@ -172,11 +216,12 @@ public class MLridesharing extends Client {
     return 1 + (100 - 1) * rand.nextDouble();
   }
 
-  protected void optimizationModule(final Integer[][] costMatrix) {
-    //if the cost matrix is sparse, it might be worth looking into the Jonker-Volgenant algorithm
-    //(https://javadoc.scijava.org/Fiji/fiji/plugin/trackmate/tracking/sparselap/linker/LAPJV.html) 
+  //used to insert a request into a vehicle's route
+  protected void addToRoute(final Object r, final int sid){
+    //TODO: we need some way to cache the insertion point when we are calculating the insertion cost
 
-    //implementation of the Hungarian algorithm which may also allow for non-square inputs
-    //https://github.com/KevinStern/software-and-algorithms/blob/master/src/main/java/blogspot/software_and_algorithms/stern_library/optimization/HungarianAlgorithm.java
+    return;    
   }
+
+
 }
