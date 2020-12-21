433659f98c7a029a37a93901a59d4f14f3f8d53d
Keith Azzopardi <keith.azzopardi.16@um.edu.mt>
2020-12-21 15:11:16 +0100

    making it easier to switch between custom and example algorithms

--- run_example_ny.sh
+++ run_example_ny.sh
@@ -9,15 +9,15 @@ ROAD="${data_dir}/mny.rnet"
 
 GTREE="${data_dir}/mny.gtree"
 
-PROB="${data_dir}/mny-9-5000.instance"
+PROB="${data_dir}/mny-1-5000.instance"
 
-#CLIENT="/home/keith/Dissertation/Jargo/jar/jargors-1.0.0.jar"
-#CLIENT="/home/keith/Dissertation/github/jargo/solvers/jar/solvers.jar"
 CLIENT="solvers/jar/solvers.jar"
-
-#CLASSNAME="com.github.jargors.sim.Client"
-#CLASSNAME="example.com.github.jargors.client.GreedyInsertion"
 CLASSNAME="Exhaustive"
+_CLASSPATH=.:jar/*:dep:dep/*:solvers:solvers/jar/*
+
+#CLIENT="example/jar/examples.jar"
+#CLASSNAME="com.github.jargors.client.GreedyInsertion"
+#_CLASSPATH=.:jar/*:dep:dep/*:example:example/jar/*
 
 #time_start=10
 #time_end=30
