1c46abae125b7445c65ad9f959256107c70f4d54
Keith Azzopardi <keith.azzopardi.16@um.edu.mt>
2020-12-21 15:10:09 +0100

    adding script to run simonetto instances

---
+++ run_example_simonetto.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+MODE="real"
+
+#data_dir="/data/dissertation_data/jargo_datasets/Manhattan"
+data_dir="${JARGO_DATA_DIR}/Simonetto"
+
+ROAD="${data_dir}/mny.rnet"
+
+GTREE="${data_dir}/mny.gtree"
+
+PROB="${data_dir}/${1}"
+
+
+CLIENT="solvers/jar/solvers.jar"
+CLASSNAME="Exhaustive"
+_CLASSPATH=.:jar/*:dep:dep/*:solvers:solvers/jar/*
+
+#CLIENT="example/jar/examples.jar"
+#CLASSNAME="com.github.jargors.client.GreedyInsertion"
+#_CLASSPATH=.:jar/*:dep:dep/*:example:example/jar/*
+
+#time_start=10
+#time_end=30
+
+echo "running in $MODE mode"
+time source ./launch-cli.sh $MODE $ROAD $GTREE $PROB $CLIENT $CLASSNAME 
