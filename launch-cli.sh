e60205e4b575937d27de19698270f1eb483048a1
Keith Azzopardi <keith.azzopardi.16@um.edu.mt>
2021-03-14 19:33:24 +0100

    disabling demand model in preparation for merge

--- launch-cli.sh
+++ launch-cli.sh
@@ -14,7 +14,7 @@ java \
     -Djargors.controller.debug=true \
     -Djargors.communicator.debug=false \
     -Djargors.client.debug=true \
-    -Djargors.client.dm_enable=true \
+    -Djargors.client.dm_enable=false \
     -Djargors.algorithm.debug=true \
     -Djargors.costcalculation.debug=false \
     -Djargors.traffic.debug=false \
