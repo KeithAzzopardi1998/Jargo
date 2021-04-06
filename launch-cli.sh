decb3529b202c7998cb298c8fdca41d9673d89ad
Keith Azzopardi <keith.azzopardi.16@um.edu.mt>
2021-04-06 12:46:44 +0200

    adding more parameters to the launch script

--- launch-cli.sh
+++ launch-cli.sh
@@ -12,11 +12,14 @@ java \
     -Dderby.storage.pageCacheSize=8000 \
     -Djargors.storage.debug=false \
     -Djargors.controller.debug=true \
+    -Djargors.controller.max_delay=7 \
+    -Djargors.controller.max_wait=7 \
     -Djargors.communicator.debug=false \
     -Djargors.client.debug=true \
     -Djargors.client.dm_enable=true \
-    -Djargors.algorithm.rebalance_enable=true \
     -Djargors.algorithm.debug=true \
+    -Djargors.algorithm.rebalance_enable=true \
+    -Djargors.algorithm.maxn=8 \
     -Djargors.costcalculation.debug=false \
     -Djargors.traffic.debug=false \
     -cp $_CLASSPATH:$DERBY_HOME/lib/derby.jar \
